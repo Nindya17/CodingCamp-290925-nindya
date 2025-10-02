94df1e1cf4406a5b4314b2a217033501cb04f76e
Nindya17 <nindyaallic17@gmail.com>
2025-10-02 22:13:12 +0700

    fisrt

---
+++ script.js
@@ -0,0 +1,17 @@
+const form = document.getElementById("todo-form");
+const taskInput = document.getElementById("task-input");
+const dateInput = document.getElementById("date-input");
+const todoList = document.getElementById("todo-list");
+
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    const task = taskInput.value;
+    const date = dateInput.value;
+
+    if(task && date) {
+
+    }
+        
+    }
+});
