8d461211e9f938965371db4b20f968a5151ab080
Nindya17 <nindyaallic17@gmail.com>
2025-10-03 21:20:38 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,34 @@
-const form = document.getElementById("todo-form");
-const taskInput = document.getElementById("task-input");
-const dateInput = document.getElementById("date-input");
-const todoList = document.getElementById("todo-list");
+console.log("Hello World");
 
-form.addEventListener("submit", function(e) {
-    e.preventDefault();
+function addTodo() {
+    const todoInput = document.getElementById("todo-input");
+    const todoDate = document.getElementById("todo-date");
+    const list = document.getElementById("todo-list");
 
-    const task = taskInput.value;
-    const date = dateInput.value;
+    form.addEventListener("submit", function(e) {
+        e.preventDefault()});
 
-    if(task && date) {
+    const task = input.value.trim();
+    const deadline = date.value;
 
-    }
+    if (todoInput.value.trim() !== "" && todoDate.value !== "") { 
         
+        const li = document.createElement("li")
+        li.textContent = `${todoInput.value} - ${todoDate.value}`;
+
+        //tombol hapus//
+        const btn = document.createElement("button");
+        btn.textContent = "x";
+        btn.style.marginLeft = "10px";
+        btn.onclick = () => li.remove();
+
+        li.appendChild(btn);
+        todoList.appendChild(li);
+
+        //reset input//
+        todoInput.value = "";
+        todoDate.value = "";
+      } else {
+        alert("Isi task dan tanggal dulu ya!");
+      }
     }
-});
